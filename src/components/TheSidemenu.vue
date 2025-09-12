5796e837362aa2176fb139d495ff857c8f1aa834
Michaël Fournier <MichaelElianF@gmx.com>
2025-09-12 09:42:57 +0200

    feat: main responsive layout with vuetify
    * clean: remove unused files
    
    * feat: add main views
    
    * feat: scaffold main app layout
    
    * feat: setup router entries
    
    * fix: clean
    
    * feat: make nav menu responsive
    
    * fix: drawer is open by default

---
+++ src/components/TheSidemenu.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+type MenuItem = {
+  slug: string
+  label: string
+  name: string
+}
+const menuItems: Ref<MenuItem[]> = ref([
+  {
+    slug: '/',
+    label: 'Etudiants',
+    name: 'home',
+  },
+  {
+    slug: '/courses',
+    label: 'Cours',
+    name: 'courses',
+  },
+  {
+    slug: '/reports',
+    label: 'Rapports',
+    name: 'reports',
+  },
+])
+</script>
+<template>
+  <v-navigation-drawer location="left" mobile-breakpoint="md">
+    <v-list max-width="360">
+      <v-list-item
+        v-for="item in menuItems"
+        color="primary"
+        :active="$route.name == item.name"
+        link
+        :title="item.label"
+        @click="$router.push(item.slug)"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
---
+++ src/components/TheTopbar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import desktopLogo from '@/assets/ulb-logo.jpg'
+import mobileLogo from '@/assets/ulb-logo-mobile.jpg'
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+const { mdAndUp } = useDisplay()
+const drawer = ref(false)
+const emit = defineEmits<{
+  (e: 'drawer'): void
+}>()
+</script>
+
+<template>
+  <v-app-bar elevation="1" height="100" class="d-flex flex-row justify-space-between">
+    <v-app-bar-nav-icon
+      v-if="!mdAndUp"
+      variant="text"
+      @click.stop="$emit('drawer')"
+    ></v-app-bar-nav-icon>
+    <div class="d-flex align-center ga-3" style="flex: 0 1 25%">
+      <RouterLink to="/" class="d-flex h-100 ml-3 mr-1">
+        <RouterLink to="/" class="d-flex ml-3 mr-1">
+          <v-img
+            contain
+            :src="desktopLogo"
+            min-width="340px"
+            inline
+            class="d-none d-sm-block"
+            style="cursor: pointer"
+          >
+          </v-img>
+          <v-img
+            contain
+            :src="mobileLogo"
+            min-width="60px"
+            inline
+            class="d-sm-none"
+            style="cursor: pointer"
+          >
+          </v-img>
+        </RouterLink>
+      </RouterLink>
+    </div>
+    <div class="d-flex justify-end mr-2" style="flex: 1 0 25%">
+      <v-avatar color="info" size="x-large">
+        <v-img alt="John" src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"></v-img>
+      </v-avatar>
+    </div>
+  </v-app-bar>
+</template>
