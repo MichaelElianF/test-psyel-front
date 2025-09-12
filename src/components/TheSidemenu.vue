b52609859101830b93e1154a86830e3129f68775
MichaelElianF <MichaelElianF@gmx.com>
2025-09-12 17:36:31 +0200

    style: add icons to menu, color, spacing improvments

--- src/components/TheSidemenu.vue
+++ src/components/TheSidemenu.vue
@@ -5,36 +5,50 @@ type MenuItem = {
   slug: string
   label: string
   name: string
+  icon?: string
 }
 const menuItems: Ref<MenuItem[]> = ref([
   {
     slug: '/',
     label: 'Etudiants',
     name: 'home',
+    icon: 'mdi-account-school-outline',
   },
   {
     slug: '/courses',
     label: 'Cours',
     name: 'courses',
+    icon: 'mdi-google-classroom',
   },
   {
     slug: '/reports',
     label: 'Rapports',
     name: 'reports',
+    icon: 'mdi-file-table-outline',
   },
 ])
 </script>
 <template>
-  <v-navigation-drawer location="left" mobile-breakpoint="md">
-    <v-list max-width="360">
+  <v-navigation-drawer
+    location="left"
+    mobile-breakpoint="md"
+    class="bg-grey-lighten-4"
+    theme="dark"
+  >
+    <v-list max-width="360" class="mt-6">
       <v-list-item
         v-for="item in menuItems"
         color="primary"
+        class="px-6"
         :active="$route.name == item.name"
         link
         :title="item.label"
         @click="$router.push(item.slug)"
-      ></v-list-item>
+      >
+        <template v-slot:prepend>
+          <v-icon size="30">{{ item.icon }}</v-icon>
+        </template>
+      </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
