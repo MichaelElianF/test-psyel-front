afe85c12f29a150c418778c1fa1a745502aa4e3e
MichaelElianF <MichaelElianF@gmx.com>
2025-09-13 15:50:37 +0200

    feat: fetch student data, compute and display values

--- src/components/students/Student.vue
+++ src/components/students/Student.vue
@@ -1,30 +1,49 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Inscription } from '@/models/Inscription'
+import type { Note } from '@/models/Note'
 import useAPI from '@/composables/useAPI'
 import { onMounted } from 'vue'
+import StudentYear from './StudentYear.vue'
 
 const props = defineProps({
   id: String,
 })
 
 const api = useAPI()
+const inscriptions = ref<Inscription[]>()
+const notesList = ref<Note[]>()
 const student = ref<Inscription>()
 
-async function fetchData() {
-  return await api.get<Inscription[]>('/inscriptions', {
-    params: { matricule: props.id },
-  })
-}
 onMounted(async () => {
-  const response = await fetchData()
-  student.value = response.data[0]
+  const params = { matricule: props.id }
+  const inscriptions_response = await api<Inscription[]>('inscriptions', { params })
+  inscriptions.value = inscriptions_response.data
+  student.value = inscriptions.value[0]
+  const notes_response = await api<Note[]>('notes', { params })
+  notesList.value = notes_response.data
 })
 </script>
 
 <template>
-  <v-card v-if="student">
-    {{ student.matricule }}
-  </v-card>
+  <template v-if="inscriptions && student">
+    <v-card> {{ student.prenom }} {{ student.nom }} </v-card>
+    <v-row>
+      <v-col
+        v-for="(inscription, index) in inscriptions"
+        :key="inscription.matricule"
+        cols="12"
+        sm="6"
+        md="4"
+      >
+        <StudentYear
+          v-if="props.id && notesList"
+          :year="inscription.annee_etude"
+          :notes-list="notesList"
+          :mnemonique-list="JSON.parse(inscription.cours_json)"
+        ></StudentYear>
+      </v-col>
+    </v-row>
+  </template>
   <div v-else>Loading...</div>
 </template>
--- src/models/Inscription.ts
+++ src/models/Inscription.ts
@@ -1,6 +1,6 @@
 export interface Inscription {
   annee_etude: number
-  cours_json: string[]
+  cours_json: string
   matricule: string
   nom: string
   prenom: string
