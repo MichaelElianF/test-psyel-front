cacf416173af92f490fe266244d95a50f84f710e
MichaelElianF <MichaelElianF@gmx.com>
2025-09-15 18:16:25 +0200

    style: add custom theme colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,5 +3,18 @@ import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
+import colors from 'vuetify/util/colors'
 
-export default createVuetify()
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: colors.indigo.base,
+          secondary: colors.deepPurple.lighten3,
+        },
+      },
+    },
+  },
+})
